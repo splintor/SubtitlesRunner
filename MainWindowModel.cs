aca1d51858a6966aad9bc015f37889fb046bfe10
Shmulik Flint <splintor@gmail.com>
2013-08-14 10:01:32 +0300

    Open file dialog
    and some more minor changes

--- MainWindowModel.cs
+++ MainWindowModel.cs
@@ -1,19 +1,44 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Microsoft.Win32;
 using SubtitlesRunner.Annotations;
 
 namespace SubtitlesRunner
 {
     public class MainWindowModel : INotifyPropertyChanged
     {
+        private List<SubtitleInfo> _subTitles;
+
         public MainWindowModel()
         {
-            CurrentSubtitle = "This is...\n...just a test";
+            //CurrentSubtitle = "This is...\n...just a test";
         }
 
-        private string _currentSubtitle;
+        public DateTime CurrentTime
+        {
+            get { return new DateTime(CurrentTimeInTicks); }
+        }
 
-        public string CurrentSubtitle
+        private long _currentTimeInTicks;
+
+        public long CurrentTimeInTicks
+        {
+            get { return _currentTimeInTicks; }
+            set
+            {
+                if (_currentTimeInTicks == value) return;
+                _currentTimeInTicks = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private SubtitleInfo _currentSubtitle;
+
+        public SubtitleInfo CurrentSubtitle
         {
             get { return _currentSubtitle; }
             set
@@ -35,6 +60,36 @@ namespace SubtitlesRunner
                 _windowTop = value;
                 OnPropertyChanged();
             }
+        }
+
+        private ICommand _openFileDialogCommand;
+
+        public ICommand OpenFileDialogCommand
+        {
+            get { return _openFileDialogCommand ?? (_openFileDialogCommand = new RelayCommand(OpenSubTitlesFileDialog)); }
+        }
+
+        private void OpenSubTitlesFileDialog(object obj)
+        {
+            var dialog = new OpenFileDialog
+                {
+                    CheckFileExists = true,
+                    DefaultExt = "srt",
+                    Filter = "Subtitles files (*.srt)|*.srt|All files (*.*)|*.*",
+                    Title = "Select Subtitles File"
+                };
+            if (dialog.ShowDialog() == true)
+            {
+                LoadSubtitlesFile(dialog.FileName);
+            }
+        }
+
+        private void LoadSubtitlesFile(string fileName)
+        {
+            _subTitles = new List<SubtitleInfo>();
+            var lines = File.ReadAllLines(fileName);
+
+            //throw new NotImplementedException();
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
---
+++ SubtitleInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SubtitlesRunner
+{
+    public class SubtitleInfo
+    {
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public string SubtitleText { get; set; }
+
+        public int Id { get; set; }
+    }
+}
